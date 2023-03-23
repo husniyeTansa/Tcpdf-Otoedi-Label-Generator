a8cdf84a8f357edfef3114db0b3885a1cf7d50a1
hakan-soyturk <117153788+hakan-soyturk@users.noreply.github.com>
2023-03-23 14:43:54 +0300

    new datas

--- index.php
+++ index.php
@@ -1,33 +1,83 @@
 <?php
 
-use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
+$jsonFileName = "C:\\xampp\htdocs\Tcpdf-Otoedi-Label-Generator\data.json";
 
-require_once __DIR__.'/vendor/autoload.php';
+print_r (count(json_decode(file_get_contents($jsonFileName))->collection));die;
+
+$labelCollection = json_decode(file_get_contents($jsonFileName));
+$despatchId = $labelCollection->despatch_id;
+$timestamp = $labelCollection->label_timestamp;
+$supplierName = $labelCollection->collection[0]->supplierName;
+$supplierGSDBCode = $labelCollection->collection[0]->supplierGSDBCode;
+$quantity = $labelCollection->collection[0]->quantity;
+$quantityPackage = $labelCollection->collection[0]->quantityPackage; // dış paket sayısı mı?
+$unitOfMeasure = $labelCollection->collection[0]->unitOfMeasure;
+$container = $labelCollection->collection[0]->container;
+
+$grossWeight = $labelCollection->collection[0]->grossWeight;
+$date = $labelCollection->collection[0]->date;
+$dateYYMMDD = $labelCollection->collection[0]->dateYYMMDD;
+$lotBatch = $labelCollection->collection[0]->lotBatch;
+$shift = $labelCollection->collection[0]->shift;
+$wc = $labelCollection->collection[0]->wc;
+$partNumber = $labelCollection->collection[0]->partNumber;
+$storageLocation = $labelCollection->collection[0]->storageLocation;
+$deliveryDocASNNumber = $labelCollection->collection[0]->deliveryDocASNNumber;
+$descriptionOfPart = $labelCollection->collection[0]->descriptionOfPart;
+$serialNumber = $labelCollection->collection[0]->serialNumber;
 
 
-$connection = new AMQPStreamConnection('10.1.1.74', 5672, 'mqadmin', 'q_9IAm-7Ieipx0PFxw_8rPpi9Y');
-$channel = $connection->channel();
+$customerPlantName = $labelCollection->collection[0]->customerPlantName;
+$customerPlantCode = $labelCollection->collection[0]->customerPlantCode;
+$customerPlantAltCode = $labelCollection->collection[0]->customerPlantAltCode;
+$customerCode = $labelCollection->collection[0]->customerCode;
+$dockCode = $labelCollection->collection[0]->dockCode;
+$engAlert = $labelCollection->collection[0]->engAlert;
+$packageQuantity = $labelCollection->collection[0]->packageQuantity;
+$is_labeled = $labelCollection->collection[0]->is_labeled;
+$isMaster = $labelCollection->collection[0]->isMaster;
 
-$channel->queue_declare('test-queue', false, false, false, false);
+
+$copy = $labelCollection->collection[0]->copy;
+$isMix = $labelCollection->collection[0]->is_mix;
+$despatchPackageId = $labelCollection->collection[0]->despatch_package_id;
+$fkDespatchPackageId = $labelCollection->collection[0]->fk_despatch_package_id;
+$packageType = $labelCollection->collection[0]->package_type;
+$packagingId = $labelCollection->collection[0]->packaging_id;
+$fkPackagingId = $labelCollection->collection[0]->fk_packaging_id;
+$labelNumber = $labelCollection->collection[0]->labelNumber;
+
+// outer package's inners
+$innerDespatchPackageId = $labelCollection->collection[0]->inners[0]->despatch_package_id;
+$fkProductPackagingId = $labelCollection->collection[0]->inners[0]->fk_product_packaging_id;
+$innerFkPackagingId = $labelCollection->collection[0]->inners[0]->fk_packaging_id;
+$fkDespatchPackageId = $labelCollection->collection[0]->inners[0]->fk_despatch_package_id;
+$fkDespatchId = $labelCollection->collection[0]->inners[0]->fk_despatch_id;
+
+$innerQuantityPackage = $labelCollection->collection[0]->inners[0]->quantity_package;
+$batchNumber = $labelCollection->collection[0]->inners[0]->batch_number;
+$innerLabelNumber = $labelCollection->collection[0]->inners[0]->label_number;
+$innerGrossWeight = $labelCollection->collection[0]->inners[0]->gross_weight;
+$netWeight = $labelCollection->collection[0]->inners[0]->net_weight;
 
 
-$callback = function ($msg) {
-   // print_r($msg->body);
-   // file_put_contents("C:\\xampp\htdocs\barcode-test\data.json", $msg->body);
-   // return view('product',['data' => json_decode($msg->body)]);
-   // dd(json_decode($msg->body));
-};
+$weightUnit = $labelCollection->collection[0]->inners[0]->weight_unit;
+$buyerCode = $labelCollection->collection[0]->inners[0]->buyer_code;
+$dockIdentifier = $labelCollection->collection[0]->inners[0]->dock_identifier;
+$internalDestination = $labelCollection->collection[0]->inners[0]->internal_destination;
+$consigneeName = $labelCollection->collection[0]->inners[0]->consignee_name;
+$consigneeIdentifier = $labelCollection->collection[0]->inners[0]->consignee_identifier;
+$consigneeAltIdentifier = $labelCollection->collection[0]->inners[0]->consignee_alt_identifier;
 
-$channel->basic_consume('test-queue', '', false, true, false, false, $callback);
-$channel->close();
-$connection->close();
 
-while ($channel->is_open()) {
-    $channel->wait();
-}
 
-die;
+$totalInner = $labelCollection->collection[0]->totalInner;
+
+
+
+
+print_r($totalInner);die;
+
 
 
 // Include the main TCPDF library (search for installation path).
@@ -63,8 +113,8 @@ $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
 $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
-// set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+// none margin bottom
+$pdf->SetAutoPageBreak(TRUE, 0);
 
 // set image scale factor
 $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
@@ -90,7 +140,7 @@ for($i = 0;$i<5; $i++){
 
     if($i%2 != 0){
         $def_loc_x = 0;
-        $def_loc_y = 132;
+        $def_loc_y = 145;
     }else{
         // add a page
         $pdf->AddPage('P', 'A4');
--- rabbit-receiver.php
+++ rabbit-receiver.php
@@ -5,23 +5,20 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-
 $connection = new AMQPStreamConnection('10.1.1.74', 5672, 'mqadmin', 'q_9IAm-7Ieipx0PFxw_8rPpi9Y');
 $channel = $connection->channel();
 
 // $channel->queue_declare('test-queue', false, false, false, false);
 
+$callback = function ($msg){
 
-$callback = function ($msg) {
-    //print_r($msg->body);
-    file_put_contents("C:\\xampp\htdocs\Tcpdf-Otoedi-Label-Generator\data.json", $msg->body);
-   // return view('product',['data' => json_decode($msg->body)]);
-   // dd(json_decode($msg->body));
+    file_put_contents("C:\\xampp\htdocs\Tcpdf-Otoedi-Label-Generator\data.json", ($msg->body));
+    exec("php C:\\xampp\htdocs\Tcpdf-Otoedi-Label-Generator\index.php");
 };
 
+
 $channel->basic_consume('test-queue', '', false, true, false, false, $callback);
-$channel->close();
-$connection->close();
+
 
 while ($channel->is_open()) {
     $channel->wait();
