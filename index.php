5ed7546082186e8efe52530d0c7905754d67e9af
hakan-soyturk <117153788+hakan-soyturk@users.noreply.github.com>
2023-03-30 11:59:07 +0300

    created sample arika-data.json

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
             <h3>Burada oluşturulan asn çeşitleri bulunmaktadır.</h3>
         </div>
         <div style="text-align:center;margin-top:30px;padding-bottom:20px">
-            <a href="/Tcpdf-Otoedi-Label-Generator-main/asn-type/plascam/index_plascam.php" class="btn btn-info">plascam_global_handling</a> <br> <br>
-            <a href="/Tcpdf-Otoedi-Label-Generator-main/asn-type/ford/index_ford.php" class="btn btn-success">ford_global_handling</a> <br> <br>
-            <a href="/Tcpdf-Otoedi-Label-Generator-main/asn-type/vda4902/index_vda4902.php" class="btn btn-danger">vda4902</a>  <br> <br>
-            <a href="/Tcpdf-Otoedi-Label-Generator-main/asn-type/odette/index_odette.php" class="btn btn-primary">label_odette_v1</a>  <br> <br>
-            <a href="/Tcpdf-Otoedi-Label-Generator-main/asn-type/jaguar/index_jaguar.php" class="btn btn-dark">Label4Jaguar</a> <br> <br>
-            <a href="/Tcpdf-Otoedi-Label-Generator-main/asn-type/arikan/index_arikan.php" class="btn btn-warning">arikan_a6</a>
+            <a href="/Tcpdf-Otoedi-Label-Generator/asn-type/plascam/index_plascam.php" class="btn btn-info">plascam_global_handling</a> <br> <br>
+            <a href="/Tcpdf-Otoedi-Label-Generator/asn-type/ford/index_ford.php" class="btn btn-success">ford_global_handling</a> <br> <br>
+            <a href="/Tcpdf-Otoedi-Label-Generator/asn-type/vda4902/index_vda4902.php" class="btn btn-danger">vda4902</a>  <br> <br>
+            <a href="/Tcpdf-Otoedi-Label-Generator/asn-type/odette/index_odette.php" class="btn btn-primary">label_odette_v1</a>  <br> <br>
+            <a href="/Tcpdf-Otoedi-Label-Generator/asn-type/jaguar/index_jaguar.php" class="btn btn-dark">Label4Jaguar</a> <br> <br>
+            <a href="/Tcpdf-Otoedi-Label-Generator/asn-type/arikan/index_arikan.php" class="btn btn-warning">arikan_a6</a>
         </div>
     </div>
 </body>
