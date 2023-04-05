bd4485ed4141ea3e369f4705c776207c4e40a72f
hakan-soyturk <117153788+hakan-soyturk@users.noreply.github.com>
2023-04-05 15:46:47 +0300

    added rabbitmq 10.1.1.90 addres

--- rabbit-receiver.php
+++ rabbit-receiver.php
@@ -4,7 +4,9 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$connection = new AMQPStreamConnection('10.1.1.74', 5672, 'mqadmin', 'q_9IAm-7Ieipx0PFxw_8rPpi9Y');
+// $connection = new AMQPStreamConnection('10.1.1.74', 5672, 'mqadmin', 'q_9IAm-7Ieipx0PFxw_8rPpi9Y');
+$connection = new AMQPStreamConnection('10.1.1.90', 5672, 'mqadmin', '8eiFxpx0PwrPq_9IAm-7I_pi9Y');
+
 $channel = $connection->channel();
 
 // $channel->queue_declare('test-queue', false, false, false, false);
